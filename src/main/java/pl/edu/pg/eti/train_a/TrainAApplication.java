9a0c61bfe1e52ec5345a38253c7b1b53f8afee37
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-10-19 13:46:36 +0200

    docs: add API description

---
+++ src/main/java/pl/edu/pg/eti/train_a/TrainAApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.eti.train_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainAApplication.class, args);
+	}
+
+}

dd5d3441ec1fef3a38cfbb56341acdc050f4b0f7
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-12-26 17:08:18 +0100

    feat: db creation with liquibase

--- train-user/src/main/java/pl/edu/pg/eti/train_a/user/controller/impl/UserDefaultController.java
+++ train-user/src/main/java/pl/edu/pg/eti/train_a/user/controller/impl/UserDefaultController.java
@@ -96,7 +96,6 @@ public class UserDefaultController implements UserController {
     @Override
     public SignInResponse signIn(SignInRequest request) {
         try {
-            System.out.println(userService.findAll());
             var user = userService.findByEmail(request.getEmail()).orElseThrow();
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(user.getUsername(), request.getPassword())
@@ -104,14 +103,9 @@ public class UserDefaultController implements UserController {
             final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
             final String jwt = jwtUtil.generateToken(userDetails.getUsername());
 
-            System.out.println("[user] JWT: " + jwt);
-
             userEventRepository.signIn(request);
-
-            System.out.println("[user] User signed in: " + user.getEmail());
             return SignInResponse.builder().token(jwt).build();
         } catch (Exception e) {
-            System.out.println("Exception: " + e.getMessage());
             throw new CustomResponseStatusException(HttpStatus.BAD_REQUEST, "userNotFound", "User is not found");
         }
     }
